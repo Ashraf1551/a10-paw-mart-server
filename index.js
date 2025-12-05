9631771dc3176d853d24b31c46708b1d1e62045c
Ashraf1551 <mdashrafgmhs@gmail.com>
2025-12-05 21:04:41 +0600

    checking the server connection

--- index.js
+++ index.js
@@ -34,4 +34,12 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Hello, Developers");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
+
 
