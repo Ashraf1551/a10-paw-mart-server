27d3806e7b3213f27bc46626cf400f1732677389
Ashraf1551 <mdashrafgmhs@gmail.com>
2025-12-05 21:02:50 +0600

    setup mongodb atlas

--- index.js
+++ index.js
@@ -6,5 +6,32 @@ const port = 3000;
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+// Cloud MongoDB (MongoDB Atlas)
+const uri =
+  "mongodb+srv://a10-pet-stuff:IwUiR5JanfQFpV6e@aae.bclhfhx.mongodb.net/?appName=aae";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
